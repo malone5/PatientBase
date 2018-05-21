5b4c23a3e94d2ab946107a16f6c665dff73a8fb0
Sean Malone <malonespm@gmail.com>
2018-05-21 15:46:13 -0700

    Updated description text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,16 +10,18 @@ class Home extends Component {
             <div className="container">
               <img src={logo} alt="logo"  width="20%" height="20%"/> 
               <p className="subtitle has-text-left">
-                PatientBase is web application build with a backend using Node.js/Express.js and a fronend using React.js. PatientBase
-                allows the user 2 major functions. (1) The ability to search a patient data set and (2) lookup specific records based on their Medicall Record Number (MRN)
+                PatientBase is a web application built with a backend using Node.js/Express.js and a front-end using React.js. 
+                PatientBase allows the user 2 major functions. (1) The ability to search a patient data set and 
+                (2) the ability to lookup specific records based on their Medical Record Number (MRN)
               </p>
               <p className="subtitle has-text-left">
-                To search patient select the [Patients] tab and type in a name. The table with automacally filter and allows the user to sort on id, name, or MRN. You can also
-                navigate straight to a patient record by clicking on their MRN.
+                To search a patient, select the [Patients] tab and type in a name. 
+                The table will automatically filter and allow the user to sort on id, name, or MRN.
+                You can also navigate straight to a patient record by clicking on their MRN.
               </p>
               <p className="subtitle has-text-left">
-                To look up a specific patient record, select the [MRM Lookup] tab, enter a valid MRN and press enter to submit.
-                If a match was found on the MRN the patient record will display.
+                To look up a specific patient record, select the [MRN Lookup] tab, enter a valid MRN and press enter to submit. 
+                If a match was found on the MRN, the patient record will display.
               </p>
             </div>
           </section>
